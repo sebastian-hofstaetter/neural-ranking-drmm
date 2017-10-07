84d899d4aa23e432f4b4d08b1342d12337a9b4db
Sebastian Hofstaetter <sebastian.hofstaetter@outlook.com>
2017-10-07 11:48:01 +0200

    Multiply results with pre-ranked results

--- neural-ranking/prepare_trec_eval.py
+++ neural-ranking/prepare_trec_eval.py
@@ -3,6 +3,16 @@ import sys
 filepath = sys.argv[1]
 filepath_out = sys.argv[2]
 
+preranked_file = sys.argv[3]
+
+preranked = [] # (topic, doc id, score) score is 0,1 if qrel
+
+with open(preranked_file, 'r') as inputFile:
+    for line in inputFile:
+        parts = line.split()
+        preranked.append((int(parts[0]), parts[2].strip(), parts[4].strip()))
+
+
 data = {}
 
 with open(filepath ,'r') as inputFile:
@@ -17,6 +27,37 @@ with open(filepath ,'r') as inputFile:
             data[topicId] = []
 
         data[topicId].append((score, docId))
+
+# prerank * neural score
+
+i_rank =0
+for topic in sorted(data):
+    list = data[topic]
+    for i in range(len(list)):
+        score, docId = list[i]
+
+        preranked_item = preranked[i_rank]
+
+        if preranked_item[0] != topic:
+            print('fixing topic unequal',preranked_item[0],topic)
+            if preranked_item[0] < topic:
+                while preranked[i_rank][0] < topic:
+                    i_rank+=1
+            preranked_item = preranked[i_rank]
+
+        if preranked_item[0] != topic or preranked_item[1] != docId:
+            print('aaahh!!',topic,preranked_item[0],docId,preranked_item[1])
+            preranked_score = next(item[2] for item in preranked if item[0] == topic and item[1] == docId)
+
+        else:
+            preranked_score = preranked_item[2] #next(item[2] for item in preranked if item[0] == topic and item[1] == docId)
+
+        #preranked_score = filter(lambda item: item[0] == topic and item[1] == docId, preranked)
+
+        list[i] = (score*float(preranked_score),docId)
+        i_rank+=1
+
+
 
 with open(filepath_out ,'w') as outFile:
     for topic in sorted(data):
